0564d90f3595f0245076260625ccd42e1ba89bdd
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2023-07-06 04:10:14 -0300

    Reworkd code for appending instead of creating new circuits

--- circuit_assembly.py
+++ circuit_assembly.py
@@ -1,33 +1,78 @@
+import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.circuit import ParameterVector
 
-def new_convolutional_layer(num_qubits, param_prefix, param_count, layer_function):
-    """Builds a convolutional layer cirquit of any number of qubits.
 
-    Parameters:
-    num_quibits (int): The number of quibits in the layer.
-    param_prefix (string): The prefix (symbol) used to identify parameters in the circuit.
-    param_count (int): Number of parameters in the layer block function
-    layer_function (function): A function that returns a 2-quibit parametric circuit that will be used in the layer.
+def new_CNN(num_qubits, ccaf):
 
-    Returns:
-    qiskit.QuantumCircuit: The convolutional layer circuit
-    """
-    qc = QuantumCircuit(num_qubits, name = "Convolutional Layer")
+    # We require the number of qubits to be even. This facilitates reasoning during circuit construction
+    # To test if the number is a odd, we use the good old bit twidling hack
+    if num_qubits == 0 or num_qubits & 1:
+        raise ValueError(
+            "num_qubits is expected to be even and grater than 0")
 
-    # TODO: Is this early bailling out correct?
-    if num_qubits == 2:
-        params = ParameterVector(param_prefix, length = param_count)
-        qc.compose(layer_function(params[0:param_count]), inplace = True)
-        qc.barrier()
-        return qc
+    # Number of convolutional / pooling layer pairs
+    num_layer_pairs = 0  # TODO
 
-    params = ParameterVector(param_prefix, length = num_qubits * param_count)
+    # The circuit with the final network
+    qcnn_circ = QuantumCircuit(num_qubits, name="QCNN")
 
-    # Iterate over pairs from bottom to top
-    for i in range(num_qubits):
-        param_slice = slice(i * param_count, (i + 1) * param_count)
-        qc.compose(layer_function(params[param_slice]), [i, (i + 1) % num_qubits], inplace = True)
-        qc.barrier()
+    # Convolutional / Pooling pair sequences construction
+    append_convolutional_layer(qcnn_circ, 0, 7, "c1", 3, ccaf)
 
-    return qc
+    return qcnn_circ
+
+
+# ccaf = convolutional circuit appending function
+# pcaf = pooling circuit appending function
+
+# https://arxiv.org/abs/1810.03787
+def append_convolutional_layer(qcnn_circ, q_start, q_end, param_prefix, param_count, ccaf, barriers=True):
+    size = q_end - q_start + 1
+
+    # The parameter verctor for this layer
+    params = ParameterVector(param_prefix, length=(size + 1) * param_count)
+    pi = 0
+
+    # Upper boundary
+    param_slice = slice(pi * param_count, (pi + 1) * param_count)
+    ccaf(qcnn_circ, q_end, q_start, params[param_slice])
+
+    if barriers:
+        qcnn_circ.barrier()
+
+    pi += 1
+
+    # Loops over odd qubit pairs (12, 34, 56, etc)
+    for i in range(q_start + 1, q_end, 2):
+        param_slice = slice(pi * param_count, (pi + 1) * param_count)
+
+        ccaf(qcnn_circ, i, i + 1, params[param_slice])
+        if barriers:
+            qcnn_circ.barrier()
+
+        pi += 1
+
+    # Lower boundary
+    param_slice = slice(pi * param_count, (pi + 1) * param_count)
+    ccaf(qcnn_circ, q_start, q_end, params[param_slice])
+
+    if barriers:
+        qcnn_circ.barrier()
+
+    pi += 1
+
+    # Loops over even qubit pairs (01, 23, 45, etc)
+    for i in range(q_start, q_end + 1, 2):
+        param_slice = slice(pi * param_count, (pi + 1) * param_count)
+
+        ccaf(qcnn_circ, i, i + 1, params[param_slice])
+
+        if barriers:
+            qcnn_circ.barrier()
+
+        pi += 1
+
+
+# indicies of qubits that need to be eliminated
+# def append_pooling_layer(qcnn_circ, q_start, q_end, param_prefix, param_count, pcaf, barriers=True):
--- unitary_blocks.py
+++ unitary_blocks.py
@@ -1,22 +1,43 @@
 import numpy as np
-from qiskit import QuantumCircuit
 
-def vatan_williams(params):
-    """Generalized 2-quibit unitary defined in https://arxiv.org/abs/quant-ph/0308006
+
+def vatan_williams(qc, q0, q1, params):
+    """Appends the generalized 2-qubit unitary defined in https://arxiv.org/abs/quant-ph/0308006
+    to an existing circuit
 
     Parameters:
-    params (qiskit.ParameterVector): Circuit parameters
-
-    Returns:
-    qiskit.QuantumCircuit: The parametric circuit block
+    qc (qiskit.QuantumCircuit): The circuit to append to.
+    q0 (int): The first qubit in the circuit.
+    q1 (int): The second qubit in the circuit.
+    params (qiskit.ParameterVector): Circuit parameters.
     """
-    qc = QuantumCircuit(2)
-    qc.rz(-np.pi / 2, 1)
-    qc.cnot(1, 0)
-    qc.rz(params[0], 0)
-    qc.ry(params[1], 1)
-    qc.cx(0, 1)
-    qc.ry(params[2], 1)
-    qc.cx(1, 0)
-    qc.rz(np.pi / 2, 0)
+
+    qc.rz(-np.pi / 2, q1)
+    qc.cnot(q1, q0)
+    qc.rz(params[0], q0)
+    qc.ry(params[1], q1)
+    qc.cx(q0, q1)
+    qc.ry(params[2], q1)
+    qc.cx(q1, q0)
+    qc.rz(np.pi / 2, q0)
+
+
+def qiskit_pooling(qc, q0, q1, params):
+    """Appennd the 2-qubit unitary for pooling used in the qiskit tutorials
+    https://qiskit.org/ecosystem/machine-learning/tutorials/11_quantum_convolutional_neural_networks.html#2.2-Pooling-Layer
+
+    Parameters:
+    qc (qiskit.QuantumCircuit): The circuit to append to.
+    q0 (int): The first qubit in the circuit.
+    q1 (int): The second qubit in the circuit.
+    params (qiskit.ParameterVector): Circuit parameters.
+    """
+
+    qc.rz(-np.pi / 2, q1)
+    qc.cnot(q1, q0)
+    qc.rz(params[0], q0)
+    qc.ry(params[1], q1)
+    qc.cnot(q0, q1)
+    qc.ry(params[2], q1)
+
     return qc
