98c2fcd4efe778238a67fc20dc35c881a73eb081
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2023-07-07 03:27:40 -0300

    Finnished QNN circuit construction

--- circuit_assembly.py
+++ circuit_assembly.py
@@ -3,64 +3,61 @@ from qiskit import QuantumCircuit
 from qiskit.circuit import ParameterVector
 
 
-def new_CNN(num_qubits, ccaf):
+def new_QCNN_circuit(num_qubits, npc, npp, ccaf, pcaf, barriers=False):
+    """ Creates a new convolutional neural network (CNN) circuit
 
-    # We require the number of qubits to be even. This facilitates reasoning during circuit construction
-    # To test if the number is a odd, we use the good old bit twidling hack
-    if num_qubits == 0 or num_qubits & 1:
+    Inputs:
+    - num_qubits (int): The total number of qubits in the NN. Must be a power of 2
+    - npc (int): The number of parameters per convolutional circuit.
+    - npp (int): The number of parameters per pooling circuit.
+    - barriers (bool): Wether or not to insert barriers between layer blocks. Default: True
+
+    Returns:
+    - qiskit.QuantumCircuit: The CNN circuit.
+    """
+
+    # We require the number of qubits to be a power of 2. This facilitates reasoning during circuit construction
+    # To test this, we use the good old bit twidling hack
+    if num_qubits == 0 or not (num_qubits & (num_qubits - 1)) == 0:
         raise ValueError(
-            "num_qubits is expected to be even and grater than 0")
+            "num_qubits is expected to be a power of 2 and grater than 0")
 
     # Number of convolutional / pooling layer pairs
-    num_layer_pairs = 0  # TODO
+    num_layer_pairs = int(np.log2(num_qubits))
 
     # The circuit with the final network
     qcnn_circ = QuantumCircuit(num_qubits, name="QCNN")
 
-    # Convolutional / Pooling pair sequences construction
-    append_convolutional_layer(qcnn_circ, 0, 7, "c1", 3, ccaf)
+    # The index of the last qubit
+    q_last = num_qubits - 1
+
+    for i in range(num_layer_pairs):
+        num_pool_prev = num_qubits - int(num_qubits / 2**i)
+        num_pool_curr = int(num_qubits / 2**(i + 1))
+        append_convolutional_layer(qcnn_circ, num_pool_prev, q_last, "c" + str(i), npc, ccaf, barriers)
+        append_pooling_layer(qcnn_circ, num_pool_prev, q_last, num_pool_curr, "p" + str(i), npp, pcaf, barriers)
 
     return qcnn_circ
 
 
-# ccaf = convolutional circuit appending function
-# pcaf = pooling circuit appending function
+def append_convolutional_layer(qcnn_circ, q_start, q_end, param_prefix, param_count, ccaf, barriers):
+    """ Appends a convolutional layer on a NN.
+    Based on https://qiskit.org/ecosystem/machine-learning/tutorials/11_quantum_convolutional_neural_networks.html
 
-# https://arxiv.org/abs/1810.03787
-def append_convolutional_layer(qcnn_circ, q_start, q_end, param_prefix, param_count, ccaf, barriers=True):
-    size = q_end - q_start + 1
+    Inputs:
+    - qnn_circ (qiskit.QuantumCircuit): The NN circuit to append to.
+    - q_start (int): Index of the first bit in the layer.
+    - q_end (int): Index of the last bit in the layer.
+    - param_prefix (str): The name of the parameters of the layer.
+    - param_count (int): Number of parameters in the CCAF.
+    - ccaf (function): Convolutional Circuit Appending Function (CCAF). The function that models the convolutional circuit.
+    - barriers (bool): Wether or not to draw barriers in the blocks.
+    """
+    layer_size = q_end - q_start + 1
 
     # The parameter verctor for this layer
-    params = ParameterVector(param_prefix, length=(size + 1) * param_count)
+    params = ParameterVector(param_prefix, length=(layer_size + 1) * param_count)
     pi = 0
-
-    # Upper boundary
-    param_slice = slice(pi * param_count, (pi + 1) * param_count)
-    ccaf(qcnn_circ, q_end, q_start, params[param_slice])
-
-    if barriers:
-        qcnn_circ.barrier()
-
-    pi += 1
-
-    # Loops over odd qubit pairs (12, 34, 56, etc)
-    for i in range(q_start + 1, q_end, 2):
-        param_slice = slice(pi * param_count, (pi + 1) * param_count)
-
-        ccaf(qcnn_circ, i, i + 1, params[param_slice])
-        if barriers:
-            qcnn_circ.barrier()
-
-        pi += 1
-
-    # Lower boundary
-    param_slice = slice(pi * param_count, (pi + 1) * param_count)
-    ccaf(qcnn_circ, q_start, q_end, params[param_slice])
-
-    if barriers:
-        qcnn_circ.barrier()
-
-    pi += 1
 
     # Loops over even qubit pairs (01, 23, 45, etc)
     for i in range(q_start, q_end + 1, 2):
@@ -73,6 +70,58 @@ def append_convolutional_layer(qcnn_circ, q_start, q_end, param_prefix, param_co
 
         pi += 1
 
+    # Loops over odd qubit pairs (12, 34, 56, etc)
+    for i in range(q_start + 1, q_end, 2):
+        param_slice = slice(pi * param_count, (pi + 1) * param_count)
 
-# indicies of qubits that need to be eliminated
-# def append_pooling_layer(qcnn_circ, q_start, q_end, param_prefix, param_count, pcaf, barriers=True):
+        ccaf(qcnn_circ, i, i + 1, params[param_slice])
+        if barriers:
+            qcnn_circ.barrier()
+
+        pi += 1
+
+    # Place circular boundary
+    param_slice = slice(pi * param_count, (pi + 1) * param_count)
+    ccaf(qcnn_circ, q_end, q_start, params[param_slice])
+
+    if barriers:
+        qcnn_circ.barrier()
+
+    pi += 1
+
+
+def append_pooling_layer(qcnn_circ, q_start, q_end, num_pooled, param_prefix, param_count, pcaf, barriers):
+    """ Appends a pooling layer on a NN.
+    Based on https://qiskit.org/ecosystem/machine-learning/tutorials/11_quantum_convolutional_neural_networks.html
+
+    Inputs:
+    - qnn_circ (qiskit.QuantumCircuit): The NN circuit to append to.
+    - q_start (int): Index of the first bit in the layer.
+    - q_end (int): Index of the last bit in the layer.
+    - num_pooled (int): Number of quibits that will be pooled (measured) in the layer.
+    - param_prefix (str): The name of the parameters of the layer.
+    - param_count (int): Number of parameters in the PCAF.
+    - pcaf (function): Pooling Circuit Appending Function (PCAF). The function that models the pooling circuit.
+    - barriers (bool): Wether or not to draw barriers in the blocks.
+    """
+    layer_size = q_end - q_start + 1
+
+    if layer_size < num_pooled:
+        raise ValueError("The size of the pooling layer is smaller than the number of qubits to pool")
+
+    # The parameter verctor for this layer
+    params = ParameterVector(param_prefix, length=num_pooled * param_count)
+    pi = 0
+
+    # The index of the quibit that is the first pooling target
+    p_start = q_start + num_pooled
+
+    # Polling circuits
+    for i in range(q_start, p_start):
+        param_slice = slice(pi * param_count, (pi + 1) * param_count)
+        pcaf(qcnn_circ, i, num_pooled + i, params[param_slice])
+
+        if barriers:
+            qcnn_circ.barrier()
+
+        pi += 1
--- main.py
+++ main.py
@@ -1,7 +1,57 @@
+from unitary_blocks import vatan_williams, qiskit_pooling
+from circuit_assembly import new_QCNN_circuit
+
+import matplotlib.pyplot as plt
+
 from qiskit import QuantumCircuit
-from qiskit.circuit import ParameterVector
 
-from unitary_blocks import vatan_williams
-from circuit_assembly import new_CNN
+from qiskit.algorithms.optimizers import L_BFGS_B
+from qiskit.circuit.library import ZFeatureMap
 
-new_CNN(8, vatan_williams) .draw(output="mpl", filename="circuit.pdf")
+from qiskit_machine_learning.algorithms.regressors import NeuralNetworkRegressor
+from qiskit_machine_learning.neural_networks import EstimatorQNN
+
+num_qubits = 4
+
+# Feature map
+feature_map = ZFeatureMap(num_qubits)
+
+# QCNN ansatz circuit
+ansatz = new_QCNN_circuit(num_qubits, 3, 3, vatan_williams, qiskit_pooling)
+
+# Full Circuit Assembly
+qcnn_qc = QuantumCircuit(num_qubits)
+qcnn_qc.compose(feature_map, inplace=True)
+qcnn_qc.compose(ansatz, inplace=True)
+qcnn_qc.draw(output="mpl", filename="circuit.pdf")
+
+
+# Estimator NN
+qcnn = EstimatorQNN(
+    circuit=qcnn_qc,
+    input_params=feature_map.parameters,
+    weight_params=ansatz.parameters,
+)
+
+
+def callback_graph(weights, obj_func_eval):
+    # callback function that draws a live plot when the .fit() method is called
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+
+
+# Regressor
+regressor = NeuralNetworkRegressor(
+    neural_network=qcnn,
+    loss="squared_error",
+    optimizer=L_BFGS_B(maxiter=100),
+    callback=callback_graph,
+)
+
+
+# draw(output="mpl", filename="circuit.pdf")
