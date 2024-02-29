833d7956fd926812e6e7a155cca8e54223a18c59
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-02-29 00:14:08 -0300

    Changed backends and diff method to speed up the code

--- src/ode_solving/harmonic_oscillator.py
+++ src/ode_solving/harmonic_oscillator.py
@@ -9,6 +9,20 @@ import h5py
 
 import os
 
+# These variables are used when using the kokkos backend
+os.environ["OMP_PROC_BIND"] = "spread"
+os.environ["OMP_PLACES"] = "threads"
+os.environ["OMP_NUM_THREADS"] = "3"
+
+num_qubits = 3
+dataset_size = 100
+
+folder_name = "harmonic_oscillator"
+
+max_iters = 5000
+abstol = 1.0e-2
+step_size = 1.0e-2
+
 font_size = 18
 line_thickness = 2.0
 line_color = "black"
@@ -113,8 +127,7 @@ def cost_int_pointwise(node, weights, x):
     return f_prime + np.sin(x)
 
 
-def cost(node, weights, data):
-    N = len(data)
+def cost(node, weights, data, N):
     f_bnd = np.abs(-1 - node(weights, x=-np.pi))
     return (np.sqrt(sum(np.abs(cost_int_pointwise(node, weights, x))**2 for x in data[1:])) + f_bnd) / N
 
@@ -122,7 +135,9 @@ def cost(node, weights, data):
 def optimize(folders: ModelFolders, circuit, device, weights, data, params: OptimizerParams):
     print("Minimizing loss")
 
-    node = qml.QNode(circuit, device)
+    # Adjoint differantiation makes the cost function computation
+    # substantially faster with lightning backends
+    node = qml.QNode(circuit, device, diff_method="adjoint")
 
     # Initial data
     cost_data = []
@@ -130,8 +145,10 @@ def optimize(folders: ModelFolders, circuit, device, weights, data, params: Opti
     opt = qml.AdagradOptimizer(params.step)
     stopping_criteria = "max iterations reached"
 
+    N = len(data)
+
     for i in range(params.max_iters):
-        c = cost(node, weights, data)
+        c = cost(node, weights, data, N)
         cost_data.append(c)
 
         print("Loss in teration", i, "=", c)
@@ -141,7 +158,7 @@ def optimize(folders: ModelFolders, circuit, device, weights, data, params: Opti
             break
         else:
             weights = opt.step(
-                lambda w: cost(node, w, data),
+                lambda w: cost(node, w, data, N),
                 weights
             )
 
@@ -154,33 +171,34 @@ def optimize(folders: ModelFolders, circuit, device, weights, data, params: Opti
     # Save Data
     save_training_data(folders, stopping_criteria, i, cost_data, weights)
 
+# Quantum circuits
 
-def process():
-    num_qubits = 3
-    dataset_size = 100
 
+def S(x):
+    for w in range(num_qubits):
+        qml.RX(x, wires=w)
+
+
+def W(theta):
+    StronglyEntanglingLayers(theta, wires=range(num_qubits))
+
+
+def entangling_circuit(weights, x=None):
+    W(weights[0])
+    S(x)
+    W(weights[1])
+    return qml.expval(qml.PauliZ(wires=0))
+
+
+def main():
     # Data folders
-    folders = ModelFolders("harmonic_oscillator")
+    folders = ModelFolders(folder_name)
 
     # Optimization params
-    params = OptimizerParams(1000, 1.0e-2, 1.0e-2)
+    params = OptimizerParams(max_iters, abstol, step_size)
 
     # Quantum device
-    device = qml.device("default.qubit", wires=num_qubits, shots=None)
-
-    # Quantum circuits
-    def S(x):
-        for w in range(num_qubits):
-            qml.RX(x, wires=w)
-
-    def W(theta):
-        StronglyEntanglingLayers(theta, wires=range(num_qubits))
-
-    def entangling_circuit(weights, x=None):
-        W(weights[0])
-        S(x)
-        W(weights[1])
-        return qml.expval(qml.PauliZ(wires=0))
+    device = qml.device("lightning.kokkos", wires=num_qubits, shots=None)
 
     # Data
     x = np.linspace(-np.pi, np.pi, num=dataset_size, endpoint=True)
@@ -204,4 +222,5 @@ def process():
         plot_circuit_function(folders, entangling_circuit, device, w, x)
 
 
-process()
+if __name__ == "__main__":
+    main()
